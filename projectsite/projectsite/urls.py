4afad520be051ea633c85b9fe6e5a9a909af9d9b
Erlybird21 <erljosephmangubat.phcs@gmail.com>
2026-08-31 16:03:25 +0800

    Commit Changes

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', views.OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<int:pk>', views.OrganizationUpdateView.as_view(), name='organization-update'),
 ]
